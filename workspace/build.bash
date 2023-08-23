57117e4fce5c36751b1b5d18c4cfb9cb6b7892cd
Takashi Mori <kanetugu2015@gmail.com>
2023-08-23 09:24:36 +0900

    add cleaner and host build

--- workspace/build.bash
+++ workspace/build.bash
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-echo "HELLO WORLD $#"
-pwd
-
-ls
-
 if [ ! -d cmake-build ]
 then
     mkdir cmake-build
 fi
 
 cd cmake-build
-cmake ..
-make
 
+if [ $# -eq 1 ]
+then
+    HAKO_CPP_SOURCE_DIR=${1}
+    if [ ! -f /usr/local/lib/hakoniwa//libhako.a ]
+    then
+        sudo cp ${HAKO_CPP_SOURCE_DIR}/cmake-build/core/src/libhako.a /usr/local/lib/hakoniwa/
+    fi
+    cmake .. -DHAKO_CPP_SOURCE_DIR=${HAKO_CPP_SOURCE_DIR}
+else
+    cmake ..
+fi
+
+make
