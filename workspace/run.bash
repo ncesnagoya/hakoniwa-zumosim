3a95278bf865ceb97a61648d58ec2710e7663ca1
Takashi Mori <kanetugu2015@gmail.com>
2023-08-23 14:26:06 +0900

    removed mqtt

--- workspace/run.bash
+++ workspace/run.bash
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-ASSET_DEF=asset_def.txt
-grep mqtt ${ASSET_DEF} > /dev/null
-HAS_MQTT=$?
-echo "ASSET_DEF=${ASSET_DEF}"
-if [ ${HAS_MQTT} -eq 0 ]
-then
-    echo "INFO: ACTIVATING MOSQUITTO"
-    mosquitto -c ./config/mosquitto.conf &
-    MQTT_PORT=1883
-    sleep 2
-else
-    MQTT_PORT=
-fi
-
-
+MQTT_PORT=
 echo "INFO: ACTIVATING HAKO-CONDUCTOR"
 hakoniwa-conductor ${DELTA_MSEC} ${MAX_DELAY_MSEC} ${CORE_IPADDR}:${GRPC_PORT} ${UDP_SRV_PORT} ${UDP_SND_PORT} ${MQTT_PORT} & 
 
