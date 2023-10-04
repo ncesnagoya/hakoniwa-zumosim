5ee1830e73c1ac5d3434a269cda3dc7431392b4f
Takashi Mori <kanetugu2015@gmail.com>
2023-10-04 10:14:56 +0900

    add api calls

--- workspace/src/apl/main_task_body.cpp
+++ workspace/src/apl/main_task_body.cpp
@@ -12,22 +12,26 @@ void apl_main_task_setup(void)
 void apl_main_task_body(void)
 {
     IZumoLED& led = zumo_get_led();
+    IZumoMotors& motors = zumo_get_motors();
     IZumoReflectanceSensorArray& rf_sensor = zumo_get_reflectance_sensor_array();
+    IZumoCompass& compass = zumo_get_compass();
 
     if (led_status) {
         led_status = false;
         led.off();
+        motors.setSpeeds(0, 5);
     }
     else {
         led_status = true;
         led.on();
+        motors.setSpeeds(1, 1);
     }
     rf_sensor.update();
     int i;
     for (i = 0; i < ZUMOSIM_REFLECT_SENSOR_NUM; i++) {
-
         std::cout << "[" << i << "] = " << rf_sensor.value(i) << std::endl;
     }
-    zumo_delay(2000);
+    std::cout << "compass: " << compass.averageHeading() << std::endl;
+    zumo_delay(5000);
     return;
 }
