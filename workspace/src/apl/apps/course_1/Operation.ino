35ec59c76d1064de551118c4ca1101b5c163afca
Takashi Mori <kanetugu2015@gmail.com>
2023-10-27 14:01:11 +0900

    add build app option

---
+++ workspace/src/apl/apps/course_1/Operation.ino
@@ -0,0 +1,101 @@
+#define MOTOR_SPEED 120
+#if 0
+#define TURN_SPEED  120
+#else
+#define TURN_SPEED  240
+#endif
+
+#define REFLECTANCE_THRESHOLD  400
+
+#define INTERSECTION_OSTIM     350
+
+#define ON_LINE(val)  ((val) > REFLECTANCE_THRESHOLD)
+
+void doOperation(void) {
+  char cmd;
+  cmd = command[ccommand++];
+
+  goStraight();
+
+  //交差点の中心まで進む
+  motors.setSpeeds(MOTOR_SPEED, MOTOR_SPEED);
+  delay(INTERSECTION_OSTIM);
+
+  //コマンド処理
+  if (cmd == '.') {
+    //停止コマンド
+    motors.setSpeeds(0, 0);
+    //buzzer.playNum(1);
+    while (1) {
+      led.on();
+      bool ret = delay(1000);
+      if (ret == false) {
+        return;
+      }
+      led.off();
+      ret = delay(1000);
+      if (ret == false) {
+        return;
+      }
+    };
+  }
+  else if ((cmd == 'r') || (cmd == 'l')) {
+    //左右へのターンコマンド
+    doTurn(cmd);
+  }
+}
+
+//直進
+void goStraight(void) {
+  motors.setSpeeds(MOTOR_SPEED, MOTOR_SPEED);
+
+  while (1) {
+    reflectances.update();
+
+    if (ON_LINE(reflectances.value(2))) {
+      motors.setSpeeds(-MOTOR_SPEED, MOTOR_SPEED);
+    } else if (ON_LINE(reflectances.value(5))) {
+      motors.setSpeeds(MOTOR_SPEED, -MOTOR_SPEED);
+    } else {
+      motors.setSpeeds(MOTOR_SPEED, MOTOR_SPEED);
+    }
+
+    //横線を見つけたら終了
+    if (ON_LINE(reflectances.value(1)) || ON_LINE(reflectances.value(6))) {
+      return;
+    }
+  }
+}
+
+//ターン
+void doTurn(char dir) {
+  int count = 0;
+  int pstate;
+
+  reflectances.update();
+
+  switch (dir) {
+    case 'r':
+      motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+      pstate = reflectances.value(5);
+      while (count < 2) {
+        reflectances.update();
+        if ((ON_LINE(reflectances.value(5)) ^ ON_LINE(pstate))) {
+          count++;
+        }
+        pstate = reflectances.value(5);
+      }
+      break;
+    case 'l':
+      motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+      pstate = reflectances.value(2);
+      while (count < 2) {
+        reflectances.update();
+        if ((ON_LINE(reflectances.value(2)) ^ ON_LINE(pstate))) {
+          count++;
+        }
+        pstate = reflectances.value(2);
+      }
+      break;
+  }
+}
