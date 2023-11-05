70231c78c9dc43a8da2c958cc70a76f18e083759
Mitsutaka Takada <mtakada@nces.i.nagoya-u.ac.jp>
2023-11-05 16:53:23 +0900

    Move Docker workdir to current directory

--- docker/dev/env.bash
+++ docker/dev/env.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export HOST_WORKDIR=`pwd`/workspace
-export DOCKER_WORKDIR=/root/workspace
+export HOST_WORKDIR=`pwd`
+export DOCKER_WORKDIR=`pwd`
 
 
 export HOST_ZUMO_INFDIR=`pwd`/hakoniwa-zumoIf/include
 export DOCKER_ZUMO_INFDIR=/root/hakoniwa-zumoIf/include
+
+export DOCKER_RUN_CMD=${DOCKER_WORKDIR}/workspace/build.bash
--- workspace/build.bash
+++ workspace/build.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 if [ ! -d cmake-build ]
 then
     mkdir cmake-build
