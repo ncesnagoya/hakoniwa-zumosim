aef70d0479c58c0e29dca014fd8390625ffb4c48
Mitsutaka Takada <mtakada@nces.i.nagoya-u.ac.jp>
2024-05-06 13:12:53 +0900

    Update Dockerfile for Zumo runtime image creation using hakoniwa conductor artifact file #22

--- docker/create-image.bash
+++ docker/create-image.bash
@@ -6,10 +6,16 @@ then
 fi
 TYPE=${1}
 
+HAKO_ARCHIVE=hakoniwa-conductor_Linux_x86_64_ubuntu-20.04.tar.gz
+
 IMAGE_NAME=`cat docker/${TYPE}/image_name.txt`
 IMAGE_TAG=`cat docker/${TYPE}/latest_version.txt`
 DOCKER_IMAGE=ncesnagoya/${IMAGE_NAME}:${IMAGE_TAG}
-DOCKER_FILE=docker/${TYPE}/Dockerfile
+if [ -f $HAKO_ARCHIVE ]; then
+    DOCKER_FILE=docker/${TYPE}/Dockerfile.hako
+else
+    DOCKER_FILE=docker/${TYPE}/Dockerfile
+fi
 
 ARCH=`arch`
 OS_TYPE=`bash docker/utils/detect_os_type.bash`
